ec78f53d5c7cfbce5c008caaf0b28ac96f177d96
kudev <kudev>
2015-11-22 17:58:28 +0100

    Add task 2 into the report

--- Assignment1/public_html/a1.js
+++ Assignment1/public_html/a1.js
@@ -2,18 +2,18 @@
  * Refreshes the top right image's position according to the current window width.
  */
 function updateTopRightImage() {
-    $('img').css('top', '0px');
-    $('img').css('right', '0px');
+    $('#task2js').css('top', '0px');
+    $('#task2js').css('right', '0px');
 }
 
 $(function () {
     // Bottom right image
-    $('body').addClass('task2');
+    $('body').addClass('task2css');
 
     // Top right image
-    $('body').append('<img src="http://www.oriooli.com/wp-content/uploads/2013/08/random-pic-01.jpg" />');
-    $('img').css('position', 'fixed');
-    $('img').css('width', '30%');
+    $('body').append('<img id="task2js" src="http://www.oriooli.com/wp-content/uploads/2013/08/random-pic-01.jpg" />');
+    $('#task2js').css('position', 'fixed');
+    $('#task2js').css('width', '15%');
     updateTopRightImage();
 });
 
