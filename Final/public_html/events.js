79a6c2af094cffc402765053dcd38fbb23a4c048
TheClam93 <rasmar93@hotmail.com>
2016-01-18 19:27:07 +0100

    Added events
    Events to highlight on timeline added

---
+++ Final/public_html/events.js
@@ -0,0 +1,49 @@
+var events = [
+    {
+        "date": "2001-01-20",
+        "enddate": "2009-01-19",
+        "desc": "George W. Bush Elected"
+    },
+    {
+        "date": "2001-9-11",
+        "desc": "9/11 Terrorist attacks"
+    },
+    {
+        "date": "2001-10-07",
+        "desc": "Invasion of Afghanistan"
+    },
+    {
+        "date": "2003-03-20",
+        "enddate": "2011-12-15",
+        "desc": "Invasion of Iraq"
+    },
+    {
+        "date": "2005-11-22",
+        "desc": "Angela Merkel becomes Chancellor of Germany"
+    },
+    {
+        "date": "2008-01-01",
+        "enddate": "20010-12-31",
+        "desc": "Financial Crisis years"
+    },
+    {
+        "date": "2009-01-20",
+        "enddate": "2009-01-19",
+        "desc": "Barack Obama Elected"
+    },
+    {
+        "date": "2011-05-02",
+        "desc": "Osama Bin Laden dies"
+    },
+    {
+        "date": "2012-07-15",
+        "desc": "Syria situation declared Civil War"
+    },
+    {
+        "date": "2012-06-21",
+        "desc": "Snowden charged with espionage"
+    },
+    {
+        "date": "2015-11-13",
+        "desc": "Paris terrorist attacks"
+    }];
