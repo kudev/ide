b8163d5af653c077b23136894f4e78b0343d9e2b
TheClam93 <rasmar93@hotmail.com>
2016-01-19 23:08:53 +0100

    Sources added
    Added sources to Events.js

--- Final/public_html/events.js
+++ Final/public_html/events.js
@@ -1,13 +1,13 @@
-var events = [
-    {
+var events = [/*
+    { 
         "date": "2001-01-20",
-        "description": "<h2>George W. Bush Elected</h2><p>The US Dollar is standing strong, leaving the 20th century. George W. Bush is elected while the dollar keeps becoming stronger.</p>",
+        "description": "<h2>George W. Bush Elected</h2><p>The US Dollar is standing strong, leaving the 20th century. George W. Bush is elected while the dollar keeps becoming stronger. <a href=\"https://en.wikipedia.org/wiki/Angela_Merkel\">Source</a></p>",
         "tooltip": "George W. Bush Elected",
         "open": 0.9
-    },
+    },*/
     {
         "date": "2001-09-11",
-        "description": "<h2>9/11</h2><p>Terrorist attacks on the world trade center fuels U.S.A. and President George W. Bush to start a \"War on Terror\" resulting in an invasion of Afghanistan and war in the middle east. The U.S. Dollar keeps falling compared to the Euro.</p>",
+        "description": "<h2>9/11</h2><p>Terrorist attacks on the world trade center fuels U.S.A. and President George W. Bush to start a \"War on Terror\" resulting in an invasion of Afghanistan and war in the middle east. The U.S. Dollar keeps falling compared to the Euro. <a href=\"https://en.wikipedia.org/wiki/September_11_attacks\">Source</a></p>",
         "tooltip": "9/11 Terrorist attacks",
         "open": 0.9
     },/*
@@ -22,13 +22,13 @@ var events = [
     },*/
     {
         "date": "2005-11-22",
-        "description": "<h2>Angela Merkel Becomes Chancellor of Germany</h2><p>The Euro has been strong for a long time, but hits it's lowest point in over 2 years. After her inauguration the Euro climbs for 3 year sup to the point where the Financial Crisis takes place.</p>",
+        "description": "<h2>Angela Merkel Becomes Chancellor of Germany</h2><p>The Euro has been strong for a long time, but hits it's lowest point in over 2 years. After her inauguration the Euro climbs for 3 years, up to the point where the Financial Crisis takes place. <a href=\"https://en.wikipedia.org/wiki/Angela_Merkel\">Source</a></p>",
         "tooltip": "Angela Merkel becomes Chancellor of Germany",
         "open": 1.2
     },
     {
         "date": "2008-01-01",
-        "description": "<h2>Financial Crisis</h2><p>Real estate market in the U.S. collapses, bringing about financial instability and recession worldwide. Obama is elected during this time, striving to reduce USA's military involvement.</p>",
+        "description": "<h2>Financial Crisis</h2><p>Real estate market in the U.S. collapses, bringing about financial instability and recession worldwide. Obama is elected during this time, striving to reduce USA's military involvement. Multiple government bailouts happened in America, artificially avoiding bankruptcy for a number of American banks. <a href=\"https://en.wikipedia.org/wiki/Great_Recession\">Source</a></p>",
         "tooltip": "Financial Crisis years",
         "open": 1.5
     },/*
@@ -42,7 +42,7 @@ var events = [
     },*/
     {
         "date": "2012-07-15",
-        "description": "<h2>Civil War in Syria</h2><p>The fighting in Syria escalates to a point where it is named a civil war. Emigration from the impacted areas increases drastically over the coming years, placing a strain upon the european recieving countries' economies.</p>",
+        "description": "<h2>Civil War in Syria</h2><p>The fighting in Syria escalates to a point where it is named a civil war. Emigration from the impacted areas increases drastically over the coming years, placing a strain upon the recieving European countries' economies.<a href=\"https://en.wikipedia.org/wiki/Syrian_Civil_War\">Source</a></p>",
         "tooltip": "Syria situation declared Civil War",
         "open": 1.3
     },
